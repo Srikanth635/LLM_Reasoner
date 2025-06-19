51642fedd66b35b5bb6f8bc0cdee3dcbf319b79b
srikanth <srikanthmalineni635@gmail.com>
2025-06-19 13:23:16 +0200

    updated df

--- start_services.sh
+++ start_services.sh
@@ -2,11 +2,13 @@
 
 echo "Starting Ollama service..."
 
-jupyter lab workspaces import ${PWD}/binder/jupyterlab.jupyterlab-workspace
-
-python3 -m flask_standalone_windows &
-
 # Start Ollama in the background
 ollama serve &
 
-wait -n
+#wait -n
+
+jupyter lab workspaces import ${PWD}/binder/jupyterlab.jupyterlab-workspace
+
+#python3 -m flask_standalone_windows &
+
+exec "$@"
